ecce0d1c88e26350898a943012689316ccdd4e61
Michael Ernst <mernst@cs.washington.edu>
2019-06-24 16:06:58 -0700

    Add import statement

--- src/test/java/org/apache/commons/csv/CSVFileParserTest.java
+++ src/test/java/org/apache/commons/csv/CSVFileParserTest.java
@@ -17,6 +17,8 @@
 
 package org.apache.commons.csv;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
